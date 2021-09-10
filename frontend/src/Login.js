7f11121c983831ed6a016fd4a809474eb6b7e6bf
gabriel <gabriel.menezes.dias@hotmail.com>
2021-09-10 11:02:57 -0300

    login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -2,8 +2,18 @@ function Login() {
   return (
     <div>
       <h1>LOGIN</h1>
-      <input type="text"></input>
-      <input type="password"></input>
+      <input
+        type="text"
+        minlength="27"
+        maxlength="35"
+      >
+      </input>
+      <input
+        type="password"
+        minlength="4"
+        maxlength="8"
+      >
+      </input>
       <button>LogIn</button>
     </div>
   )
