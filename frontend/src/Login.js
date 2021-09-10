303f1cbec99f52cb89b3e168b9c27c516b8b304a
gabriel <gabriel.menezes.dias@hotmail.com>
2021-09-10 10:56:08 -0300

    login

---
+++ frontend/src/Login.js
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <input type="text"></input>
+      <input type="password"></input>
+      <button>LogIn</button>
+    </div>
+  )
+}
+
+export default Login;
