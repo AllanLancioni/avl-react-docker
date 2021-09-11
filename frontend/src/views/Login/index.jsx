bc9098c40786d2bca5b6752e8d6ef22eebc1a988
gabriel <gabriel.menezes.dias@hotmail.com>
2021-09-11 13:16:08 -0300

    gabz

--- frontend/src/views/Login/index.jsx
+++ frontend/src/views/Login/index.jsx
@@ -1,20 +1,48 @@
+import { useState } from "react";
+import Header from "../../components/Header";
+import './index.css';
+
 function Login() {
+
+  const [passwordInvalid, setPasswordInvalid] = useState(true);
+  const [emailInvalid, setEmailInvalid] = useState(true);
+
+  const handleEmail = (event) => {
+    const re = /\w+@trademaster.com.br/;
+    if (re.test(event.target.value)) {
+      setEmailInvalid(false);
+    }
+  };
+
+  const handlePassword = (event) => {
+    const password = event.target.value;
+    const MIN_LENGTH_PASSWORD = 4;
+    const MAX_LENGTH_PASSWORD = 8;
+    if (password.length >= MIN_LENGTH_PASSWORD && password.length <= MAX_LENGTH_PASSWORD) {
+      setPasswordInvalid(false);
+    }
+  };
+
   return (
     <div>
-      <h1>LOGIN</h1>
+      <Header />
+      <h1>Entrar</h1>
       <input
+        name="email"
         type="text"
-        minlength="27"
-        maxlength="35"
+        onChange={ handleEmail }
       >
       </input>
       <input
         type="password"
-        minlength="4"
-        maxlength="8"
+        onChange={ handlePassword }
       >
       </input>
-      <button>LogIn</button>
+      <button
+        disabled={ emailInvalid || passwordInvalid }
+      >
+        Login
+      </button>
     </div>
   )
 }
